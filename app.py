f319289469e8ec7b3248d611dc68b6148fda17e8
Zorro6191 <minhajmumtaz1947@gmail.com>
2021-11-09 19:16:03 +0500

    Fixed the video problem

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ path.replace('"\"','"\\"')
 
 imageRange = int(input('Input the total number of images: '))
 
-driver = webdriver.Chrome(executable_path="C:\\Users\\Lenovo X1 Carbon\\Downloads\\chromedriver.exe")
+driver = webdriver.Chrome(executable_path="C:\\Users\\Lenovo X1 Carbon\\Documents\\Business\\App Development\\VRBO-Scrapper\\vrbo-bulk-image-scrapper\\chromedriver.exe")
 driver.get(url)
 
 images = driver.find_element_by_class_name('pdp-layout__top-content')
@@ -26,6 +26,8 @@ time.sleep(2)
 button = driver.find_element_by_xpath('.//*[@id="photos"]/div[2]/div[2]/div[2]/button')
 
 for image in range(imageRange):
+    
+
     if (image+1) == 2 or (image+1) == 1:
         img = driver.find_element_by_xpath('.//*[@id="photos"]/div[2]/div[2]/ol/li['+str(image+1)+']/img')
     elif image == imageRange-2:
@@ -33,6 +35,14 @@ for image in range(imageRange):
     elif image == imageRange-1:
         img = driver.find_element_by_xpath('.//*[@id="photos"]/div[2]/div[2]/ol/li[5]/img')
     else:
+        try:
+            driver.find_element_by_xpath('.//*[@id="photos"]/div[2]/div[2]/ol/li[3]/img')
+        except:
+            print('Video is here')
+            image=image+1
+            button.click()
+            time.sleep(1)
+        
         img = driver.find_element_by_xpath('.//*[@id="photos"]/div[2]/div[2]/ol/li[3]/img')
 
     src = img.get_attribute('src')
